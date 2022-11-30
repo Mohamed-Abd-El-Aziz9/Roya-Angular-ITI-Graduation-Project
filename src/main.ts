91055eb675573a06e8231c87954be9237aed1099
A7med-Hamdi <ahmedhamdy102020@gmail.com>
2022-11-30 12:51:52 +0200

    dartest

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
+//tesst 123141243
 //i am ahmef
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
